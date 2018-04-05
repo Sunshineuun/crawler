45a441bb82397ffb9ceb438d45f3762928032732
MinNie930625 <qiushengming@aliyun.com>
2018-04-05 16:14:38 +0800

    null

--- python/work/temp.py
+++ python/work/temp.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # encoding: utf-8
 # qiushengming-minnie
+import csv
+import datetime
+
 import pymongo
 
+from python.no_work.crawler.realty import LianJia
 from python.no_work.utils.excel import WriteXLSX
 from python.no_work.utils.mongodb import MongodbCursor
 from python.no_work.utils.crawler import Crawler
-from python.work.crawler.yaozh import yaozh_monitored
+from python.work.crawler.yaozh import yaozh_monitored, yaozh_zy, yaozh_unlabeleduse
+
+mongo = pymongo.MongoClient('192.168.5.94', 27017)
 
 
 def get_key(dbname):
-    mongo = pymongo.MongoClient('192.168.16.113', 27017)
     cursor = mongo[dbname]['data']
     title = []
     for data in cursor.find():
@@ -21,17 +26,16 @@ def get_key(dbname):
     return title
 
 
-def count_url(name, ip='192.168.5.94'):
+def count_url(name):
     """
     统计以抓取未抓取的url地址
     :param name:
     :param ip:
     :return:
     """
-    mongo = MongodbCursor(ip)
-    html_cursor = mongo.get_cursor(name, 'html')
-    url_cursor = mongo.get_cursor(name, 'url')
-    data_cursor = mongo.get_cursor(name, 'data')
+    html_cursor = mongo[name]['html']
+    url_cursor = mongo[name]['url']
+    data_cursor = mongo[name]['data']
 
     print('html-总数:', html_cursor.find().count())
     print('url-总数:', url_cursor.find().count())
@@ -40,29 +44,53 @@ def count_url(name, ip='192.168.5.94'):
     print('data-解析的数量', data_cursor.find().count())
 
 
-def proxy():
-    c = Crawler()
-    html = c.request_get_url(
-        'https://sh.lianjia.com/api/newhouserecommend?type=1&query=https://sh.lianjia.com/ershoufang/pg3/').decode(
-        'utf-8')
-    print(1)
+def yaozh_monitored_count():
+    """
+    统计每个月的数量
+    :return:
+    """
+    cursur = mongo['yaozh_monitored']['data']
+    end = datetime.datetime.now()
+    year = 2015
+    month = 8
+    while True:
+        month += 1
+        if month > 12:
+            month = 1
+            year += 1
+        start = datetime.datetime(year, month, 1)
+        print(
+            start.strftime('%Y-%m'),
+            cursur.find({'日期': {'$regex': start.strftime('%Y-%m')}}).count()
+        )
+        if start > end:
+            break
 
 
 if __name__ == '__main__':
-    # y = yaozh_zy(ip='192.168.16.113')
+    """
+    """
+    # yaozh_monitored_count()
+    # y = yaozh_zy(ip='192.168.5.94')
     # y.parser()
+    # w = WriteXLSX(path='D://Temp//药智网_中药药材_yaozh_zy.xlsx')
+    # w.write('yaozh_zy', 'data')
 
     # y1 = yaozh_zyfj(ip='192.168.16.113')
     # y1.parser()
 
     # y2 = yaozh_interaction(ip='192.168.16.113')
     # y2.parser()
+    # w = WriteXLSX(path='D://Temp//药智网_药品相互作用_interaction.xlsx')
+    # w.write('yaozh_interaction', 'data')
 
-    y3 = yaozh_monitored('192.168.5.94')
-    y3.startup()
+    # y3 = yaozh_monitored('192.168.5.94')
+    # y3.startup()
+    # w = WriteXLSX(path='D://Temp//药智网_辅助与重点监控用药_yaozh_monitored.xlsx')
+    # w.write('yaozh_monitored', 'data')
 
-    # w = WriteXLSX(path='D://Temp//CFDA.xlsx')
-    # w.write('cfda', 'data')
+    y4 = yaozh_unlabeleduse('192.168.5.94')
+    y4.startup()
 
     # get_key('cfda')
     # count_url('cfda1')
