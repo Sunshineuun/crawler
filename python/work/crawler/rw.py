2068736e7ce0e58fc535cece9162bdbd332b8f7b
MinNie930625 <qiushengming@aliyun.com>
2018-04-06 20:10:21 +0800

    null

--- python/work/crawler/rw.py
+++ python/work/crawler/rw.py
@@ -3,14 +3,15 @@
 # qiushengming-minnie
 # 人卫助手
 import json
+import random
 
 import requests
 from bs4 import BeautifulSoup
 
 from python.no_work.crawler.base_crawler import BaseCrawler
-from python.no_work.utils import mlogger
+from python.no_work.utils import mlogger, PROXY_IP
 
-logger = mlogger.get_defalut_logger('yaozhi.log', 'yaozhi')
+logger = mlogger.get_defalut_logger('rw.log', 'rw')
 
 
 class disease(BaseCrawler):
@@ -19,23 +20,17 @@ class disease(BaseCrawler):
     """
 
     def _init_url(self):
-        params = {
-            'pageNo': 1,
-            'pageSize': 100,
-            'searchText': '',
-            'icd10Code': '',
-            'departmentCode': '',
-            'symptomsWords': ''
-        }
+
+        if self._urlpool.find_all_count():
+            return
 
         url = 'http://ccdas.ipmph.com/rwDisease/getRwDiseaseList'
         result = []
         for page in range(1, 98):
-            params['pageNo'] += page
             result.append({
                 'url': url,
                 'type': self._get_cn_name(),
-                'params': str(params)
+                'pageNo': page
             })
         self._urlpool.save_url(result)
 
@@ -48,14 +43,23 @@ class disease(BaseCrawler):
     def startup(self):
         header = {
             'Content-Type': 'application/json',
-            'Cookie': 'jeesite.session.id=1ab0769d482c4068bac4d2a6b3ecec74; JSESSIONID=206209EF59D3A574B729D798F1A607B5'
+            # 'Cookie': 'jeesite.session.id=1ab0769d482c4068bac4d2a6b3ecec74; JSESSIONID=206209EF59D3A574B729D798F1A607B5'
+        }
+        params = {
+            'pageNo': 1,
+            'pageSize': 100,
+            'searchText': '',
+            'icd10Code': '',
+            'departmentCode': '',
+            'symptomsWords': ''
         }
         url1 = 'http://ccdas.ipmph.com/rwDisease/getRwDiseaseDetail?diseaseId={diseaseId}'
         while not self._urlpool.empty():
             data = self._urlpool.get()
 
-            if 'params' in data:
-                res = requests.post(url=data['url'], data=json.dumps(data['params']), headers=header)
+            if 'pageNo' in data:
+                params['pageNo'] = data['pageNo']
+                res = requests.post(url=data['url'], data=json.dumps(params), headers=header)
                 if res.status_code == requests.codes.ok:
                     res_dic = res.json()
                     self._html_cursor.insert_one(res_dic)
